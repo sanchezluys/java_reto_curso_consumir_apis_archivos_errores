8c288e9eb2ab49055c09c5114bb05959ca23984a
luys <sanchezluys@gmail.com>
2024-04-29 12:08:22 -0500

    se coloca la clase principal con busqueda definido en el curso, se observa error en las comillas.

---
+++ PrincipalConBusqueda.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el nombre de una pelicula: ");
+        var busqueda = lectura.nextLine();
+
+        String clave = "5e85123f";
+        String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=" + clave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
+
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # JAVA ALURA LATAM USO DE APIS, ARCHIVOS Y CONTROL DE ERRORES 
 Versiones
 
-| Versión | Descripción              | Bugs |
-|---------|--------------------------|------|
-| 1        | Alfa, inicializando todo | n/a  |
-|         |                          |      |
-|         |                          |      |
+| Versión | Descripción                                                     | Bugs |
+|---------|-----------------------------------------------------------------|------|
+| 1        | Alfa, inicializando todo                                        | n/a  |
+|         | Se observa error en las comillas del archivo original del curso |      |
+|         |                                                                 |      |
